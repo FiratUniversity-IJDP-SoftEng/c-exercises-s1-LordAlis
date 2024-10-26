2a867e471dd30fdbf4fb5134d76ff6c477b022d5
LordAlis <alicagancebeci@gmail.com>
2024-10-27 01:40:56 +0300

    Update ex1.c , looks finished
    I did the game with scanf like what the readme hint says. The game says you're right when it's three, and you're wrong when you say otherwise. Code is compiled and works smooth according to my tests. If you find any issues, please let me know, I'll try to fix it.

--- ex1.c
+++ ex1.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 
+int main() {
+    int guess;
+    int secretNumber = 3;
+
+    printf("Guess a number between 1 and 5: ");
+    scanf("%d", &guess);
+
+    if (guess == secretNumber) {
+        printf("Well done! You guessed it right!\n");
+    } else {
+        printf("Sorry, it's wrong, you lost. The correct number was %d.\n", secretNumber);
+    }
+
+    
+}
