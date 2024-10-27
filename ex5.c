d8985e86fe7392202f66cc6a8c504bbe8b8f9443
LordAlis <alicagancebeci@gmail.com>
2024-10-27 03:39:46 +0300

    Update ex5.c
    The program I made, firstly asks the user for their name, and secondly, uses it to print a personalized greeting message.  Please let me know if any corrections needed So I can work on code,  Thanks for reviewing my code and for your time, Prof.

--- ex5.c
+++ ex5.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+void sayHello(char name[]) {
+    printf("Hello, %s!\n", name);
+}
+
+int main() {
+    char name[30];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    sayHello(name);
+
+}
